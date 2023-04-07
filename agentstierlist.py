6a9a0a54b03ffa0391b7bd177d619ff721fc53b9
AleXDasteR <alexx_i@mail.ru>
2023-04-07 21:30:58 +0300

    printt

--- agentstierlist.py
+++ agentstierlist.py
@@ -10,8 +10,8 @@ def trackerparse():
     print('Agent     PickRate Win%   Kills  KDR')
     for i in range(0, 105, 5):
         print(
-            agents[i].text.ljust(9) + agents[i + 1].text.ljust(8) + agents[i + 2].text.ljust(6) +
-            agents[i + 3].text.ljust(6) + agents[i + 4].text)
+            agents[i].text.ljust(9), agents[i + 1].text.ljust(8), agents[i + 2].text.ljust(6),
+            agents[i + 3].text.ljust(6), agents[i + 4].text)
 
 
 trackerparse()
