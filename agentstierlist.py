a8ca6834f5daa35a772d36b68ef26dcd0f529415
AleXDasteR <alexx_i@mail.ru>
2023-04-07 20:23:24 +0300

    printt

--- agentstierlist.py
+++ agentstierlist.py
@@ -1,16 +1,25 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import telebot
+
+# region
+token = '5611338157:AAFAZ0Jh7F-70b5RRJ_-3_pr3Y-X-zjxnJM'
+bot = telebot.TeleBot(token)
 
 
-def trackerparse():
+# endregion
+
+@bot.message_handler(commands=['tracker'])
+def trackerparse(message):
     url = 'https://tracker.gg/valorant/insights/agents'
     request = requests.get(url)
     soup = bs(request.text, 'html.parser')
     agents = soup.find_all('div', class_='value')
-    print('Agent     PickRate Win%   Kills  KDR')
+    bot.send_message(message.chat.id, 'Agent     PickRate Win%   Kills  KDR')
     for i in range(0, 105, 5):
-        print(agents[i].text.ljust(9), agents[i + 1].text.ljust(8), agents[i + 2].text.ljust(6),
-              agents[i + 3].text.ljust(6), agents[i + 4].text)
+        bot.send_message(message.chat.id,
+                         agents[i].text.ljust(9) + agents[i + 1].text.ljust(8) + agents[i + 2].text.ljust(6) +
+                         agents[i + 3].text.ljust(6) + agents[i + 4].text)
 
 
 trackerparse()
--- mainbots.py
+++ mainbots.py
@@ -4,16 +4,35 @@ from perfectpickchoice import mappick as pick
 from solopickbyclass import solopick as solo
 from solopickbyclass import klassi as k
 from solopickbyclass import maps as m
+import requests
+from bs4 import BeautifulSoup as Bs
+
 # region
 token = '5611338157:AAFAZ0Jh7F-70b5RRJ_-3_pr3Y-X-zjxnJM'
 bot = telebot.TeleBot(token)
+
+
 # endregion
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id,
-                     "Привет, я бот, дающий идеальные пики на карты, введи название карты на английском и я дам тебе идеальный пик на карту)")
+                     "Привет, я бот, дающий идеальные пики на карты, введи название карты на "
+                     "английском и я дам тебе идеальный пик на карту или введи запрос в виде роль+map")
+
+
+@bot.message_handler(commands=['tracker'])
+def trackerparse(message):
+    url = 'https://tracker.gg/valorant/insights/agents'
+    request = requests.get(url)
+    soup = Bs(request.text, 'html.parser')
+    agents = soup.find_all('div', class_='value')
+    bot.send_message(message.chat.id, 'Agent     PickRate Win%   Kills  KDR')
+    for i in range(0, 105, 5):
+        bot.send_message(message.chat.id,
+                         agents[i].text.ljust(9) + agents[i + 1].text.center(8) + agents[i + 2].text.ljust(6) +
+                         agents[i + 3].text.ljust(6) + agents[i + 4].text)
 
 
 @bot.message_handler(content_types=['text'])
